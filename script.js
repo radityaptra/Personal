9e016fb89ab99811b1c0816c62af6943bef68f84
Sir Naomi <gdevelop351@gmail.com>
2024-08-10 19:30:54 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var typing=new Typed(".teks-berjalan", {
+    strings: [ "Web Designer", "Freelancer", "Graphics Designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+});
