79b051fcd26ee38f535f8041fcbda87653908002
Sir Naomi <gdevelop351@gmail.com>
2024-08-15 22:24:04 +0800

    update paragraf

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const typed = new Typed('.teks-bergerak', {
-    strings: ['Front End','Back End','Full Stack'],
+    strings: ['Frontend Developer','Backend Developer','Full Stack Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 100,
